32ebef19b679118175e0ac53b5258e0ee50624e8
oscar1223 <oscararauzp@gmail.com>
2024-12-12 19:19:47 +0100

    debug

--- axmed_catalogue/settings.py
+++ axmed_catalogue/settings.py
@@ -13,22 +13,28 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import environ
+
+env = environ.Env(
+    # set casting, default value
+   
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-&ajf$!ceirrxd8-t4kz6qf=nms@u$h!bttto(n7%zba41*q)r9')
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = False
+DEBUG = env('DEBUG')
 
 # ALLOWED_HOSTS = [
 #     'https://kc63ppdg7e.eu-west-1.awsapprunner.com',            
@@ -36,7 +42,7 @@ DEBUG = False
 #     '127.0.0.1',
 # ]
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -174,7 +180,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')  # Usando Redis como broker
+CELERY_BROKER_URL = env('CELERY_BROKER_URL')  # Usando Redis como broker
 CELERY_BEAT_SCHEDULE = {
     'send_daily_email': {
         'task': 'catalogue.tasks.send_daily_email',
@@ -185,10 +191,10 @@ CELERY_BEAT_SCHEDULE = {
 # Emails
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
