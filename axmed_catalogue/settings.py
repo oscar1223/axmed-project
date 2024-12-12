844e35b46e691b4de8bc33d4027a92d75958969f
oscar1223 <oscararauzp@gmail.com>
2024-12-12 16:13:35 +0100

    CORS_ALLOWED_ORIGIN updating 2.0

--- axmed_catalogue/settings.py
+++ axmed_catalogue/settings.py
@@ -73,7 +73,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = 'https://kc63ppdg7e.eu-west-1.awsapprunner.com/'
+CORS_ALLOWED_ORIGINS = ['https://kc63ppdg7e.eu-west-1.awsapprunner.com/', 
+                        'http://localhost:8000'
+]
 
 ROOT_URLCONF = 'axmed_catalogue.urls'
 
