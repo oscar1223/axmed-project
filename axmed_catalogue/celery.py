9604a845f553e123b77530a2c1e5586c7eee120c
oscar1223 <oscararauzp@gmail.com>
2024-12-18 11:49:00 +0100

    trading bot

--- axmed_catalogue/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'axmed_catalogue.settings')
-
-app = Celery('axmed_catalogue')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
